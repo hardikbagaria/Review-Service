21656c98350667aefd2e4d54510f94e98e3c4dfb
hardik <hardikbagaria0@gmail.com>
2026-05-09 21:16:44 +0530

    Created BoilerPlate Code for Register Service
    Signed-off-by: hardik <hardikbagaria0@gmail.com>

---
+++ login-register-service/src/test/java/com/hardikcodes/loginregisterservice/LoginRegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardikcodes.loginregisterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegisterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
